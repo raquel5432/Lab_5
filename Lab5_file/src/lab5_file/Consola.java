fa3b9e89045792f8ac992095cfc2c6012ad09c79
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2025-11-21 09:51:49 -0600

    avance

--- Lab5_file/src/lab5_file/Consola.java
+++ Lab5_file/src/lab5_file/Consola.java
@@ -4,6 +4,8 @@
  */
 package lab5_file;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ALISSONRAQUELMARTINE
@@ -13,6 +15,31 @@ public class Consola {
     public static Nodo root;
     public static Nodo actual;
     
-    
-    
+     public static void main(String[] args) {
+         
+        root = new Nodo("C:",false,"",null);
+        
+        actual = root;
+        
+        Scanner sc = new Scanner(System.in); 
+        
+         System.out.println("____Bienvenidos____");
+         
+         while(true){
+             System.out.println(rutaActual()+">");
+             String comando =sc.nextLine();
+         }
+         
+        
+        
+     }
+     public static String rutaActual(){
+         
+         Nodo temp = actual;
+         String ruta = temp.nombre;
+         
+           while (temp.padre != null) { 
+               
+           }
+         }
 }
